62c18a2fd7c0036686e2efdb6a08f308d0cb3d6d
B-Shankar <learn.bhimashankar@gmail.com>
2025-09-28 19:48:22 +0530

    PEP8

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
 print("Hello World!")
+
+print("Hi Cat, 😺")
+
+print("*" * 12)
+print(("*" * 12).count("*"))
+
+print('')
+print("new line")
+
+# PEP8: Python Enhancement Proposals
+# Formatting shortcut: 'CTRL + ALT + L'
+a = 10
+b = 20
+
