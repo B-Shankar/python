735943fa548a93395ed56f5057d0c4c89a5f930b
B-Shankar <learn.bhimashankar@gmail.com>
2025-09-30 00:04:48 +0530

    Comparison Operator in Python

---
+++ src/operators/comparison_operators/main.py
@@ -0,0 +1,27 @@
+# Comparison Operator
+
+# Greater Than
+print(10 > 3)
+
+# Less Than
+print(1 < 3)
+
+# Less than or Equal
+print(3 <= 5)
+
+# Greater than oe Equal
+print(5 >= 3)
+
+# is Equal
+print(1 == 10 ** 0)
+print(10 == 10)
+print(10 == "10")
+
+# is Not Equal
+print(2 != 10 ** 0)
+
+print("ball" > "apple")  # After sort, ball comes after apple. So, ball is greater
+print("bag" == "BAG")  # Both are not same.
+
+print(ord("b"))
+print(ord("B"))
