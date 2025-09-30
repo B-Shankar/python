95f92630486643a63cc869722cecc5de794e5c02
B-Shankar <learn.bhimashankar@gmail.com>
2025-10-01 00:10:34 +0530

    Chaining Comparison Operator

---
+++ src/operators/chaining_comparison_operator/main.py
@@ -0,0 +1,11 @@
+# Chaining Comparison operator
+
+# Age should be between 18 to 65
+age = 22
+
+if 18 <= age < 65:  # same as 'if age >= 18 and age < 65:'
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+# Directly we use how we represents in maths, 18 < age < 65
