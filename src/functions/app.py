4e190e98918edb73d35686dfca424c51a8260054
B-Shankar <learn.bhimashankar@gmail.com>
2025-10-02 20:36:13 +0530

    functions

---
+++ src/functions/app.py
@@ -0,0 +1,12 @@
+# Functions
+
+def greet():
+    print("Hello, world!")
+
+
+# greet()
+
+# This return
+# 1. Hello, world!
+# 2. None
+print(greet())  # in python all function return None Object by default, if nothing is returned by the function
